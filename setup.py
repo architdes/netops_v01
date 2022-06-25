a11b88d16d2c1b048058268011135c76cbdfbbeb
architdes <archit.desai@thermofisher.com>
2022-06-25 00:00:38 -0400

    initial-commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+import maskpass
+
+def main():
+    #username = input("Enter the username: ")
+    #password = maskpass.askpass()
+    #host = input("Enter the hostname: ")
+    username = 'adesai'
+    password = 'QAZX!@34qazx'
+    host = '10.0.0.31'
+
+if __name__ == "__main__":
+    main()
